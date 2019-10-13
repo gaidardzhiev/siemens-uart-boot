fbaaf35d60e8d784a268b886c015b6d8c84b702e
gaidardzhiev <1v4n@protonmail.com>
2019-10-13 06:53:58 +0300

    Update and rename main.c to hello.c

--- hello.c
+++ hello.c
@@ -1,14 +1,14 @@
 #include <pmb8876.h>
 
-const char test_const_string[] = "Hello World!\n";
-char test_rw_string[] = "Hello World!\n";
+const char test_const_string[] = "hello!\n";
+char test_rw_string[] = "hello!\n";
 
 int a = 234434;
 const aa = 433443332;
 
 void test() {
 	if (a == 234434 && aa == 433443332) {
-		pmb8876_serial_print("Hello World!\n");
+		pmb8876_serial_print("hello!\n");
 		pmb8876_serial_print(test_const_string);
 		pmb8876_serial_print(test_rw_string);
 		test_rw_string[1] = 'a';
